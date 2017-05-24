1e72e4784393b42f49d157b6f0392fad8082f91a
Pitso Phera <pitsophera@gmail.com>
2017-05-24 20:42:09 +0200

    Add searchBooks method

--- src/app/google-book-api.service.ts
+++ src/app/google-book-api.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Http, Response} from '@angular/http';
+import 'rxjs/Rx';
 
 @Injectable()
 export class GoogleBookApiService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
+
+  searchBooks(search) {
+    const encodedURI  = encodeURI("https://www.googleapis.com/books/v1/volumes?q="+ search +"&maxResults=12")
+    return this.http.get(encodedURI)
+        .map((response: Response) => response.json());
+  }
 }
